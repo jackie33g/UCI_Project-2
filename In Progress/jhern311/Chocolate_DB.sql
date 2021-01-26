67b1b4524b054925389c466a26e0e43307c13459
jhern311 <jhern311@yahoo.com>
2021-01-25 22:14:48 -0800

    updating

--- In Progress/jhern311/Chocolate_DB.sql
+++ In Progress/jhern311/Chocolate_DB.sql
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-Alter Table donut_top10
-Add id_num Serial
-
-Alter Table donut_top10
-Add CONSTRAINT PK_dependency_num PRIMARY KEY (id_num)
-
-SELect * FROM donut_top10
-=======
 SELECT * FROM id_df
->>>>>>> 61461a35b544b9fc8f56f580eebc2eceecf3b1a0
